9cebb91fa242d009dfb0eb0345ac4dd4e96e4e13
Marcus Öhman <marcus.ohman@mtgx.se>
2015-06-10 16:14:29 +0200

    testing enable sockets

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var app = require('express')();
 var http = require('http').Server(app);
-//var io = require('socket.io')(http);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-/*io.on('connection', function(socket){
+io.on('connection', function(socket){
   console.log('a user connected');
-});*/
+});
 
 var port = process.env.PORT || 3000;
 
