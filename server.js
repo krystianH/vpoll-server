cdc406bb1843c80df2e9b63b530e66537a7189d7
Marcus Öhman <marcus.ohman@mtgx.se>
2015-06-10 15:23:55 +0200

    testing if socket crashes heroku

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
-io.on('connection', function(socket){
+/*io.on('connection', function(socket){
   console.log('a user connected');
-});
+});*/
 
 var port = process.env.PORT || 3000;
 
 http.listen(port, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:'+port);
 });
